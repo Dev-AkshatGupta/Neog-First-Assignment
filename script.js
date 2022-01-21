86eb12ccff019a92d2b97736b08e6ab5ebf4c0d1
anuakshat <53713926+anuakshat@users.noreply.github.com>
2022-01-21 08:57:51 +0530

    Styles for tabs bar updated

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ drawerListItems.forEach(drawerItem =>{
         drawerListItems.forEach(item => item.classList.remove("active"));
         drawerItem.classList.add("active");
     })});
+
+    // active class addition in tabs
+    const tabsListItems = document.querySelectorAll(".tabs-div-ul-li ");
+
+tabsListItems.forEach(tabsItem =>{
+    tabsItem.addEventListener("click", () => {
+        tabsListItems.forEach(item => item.classList.remove("active-tabs"));
+        tabsItem.classList.add("active-tabs");
+    })});
